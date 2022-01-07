d075c8221f2f87b81057a540ef89e41f09868038
Carlos Railson <carlosrailsondev@gmail.com>
2022-01-07 19:02:34 -0300

    updating

--- art/js/main.js
+++ art/js/main.js
@@ -6,3 +6,29 @@ function toggleMenu() {
 }
 
 menuMobile.addEventListener('click', toggleMenu)
+
+const canecaAAAP = document.querySelector('.porcelana-aaa-p')
+const canecaPO = document.querySelector('.polimero-p')
+const camisa = document.querySelector('.poliester-p')
+
+function porcelana() {
+  removerBlock()
+  canecaAAAP.style.display = 'block'
+}
+
+function polimero() {
+  removerBlock()
+  canecaPO.style.display = 'block'
+}
+
+function poliester() {
+  removerBlock()
+  camisa.style.display = 'block'
+}
+
+function removerBlock() {
+  canecaAAAP.style.display = 'none'
+  canecaPO.style.display = 'none'
+  camisa.style.display = 'none'
+
+}
